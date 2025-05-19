3816db922f1073f18612e5da3f42ba1b628b316f
ruben <ruben.juanez@estudiantat.upc.edu>
2025-05-19 21:32:08 +0200

    Arreglo

--- src/grass_drone_camera/grass_drone_camera/send_images.py
+++ src/grass_drone_camera/grass_drone_camera/send_images.py
@@ -1,35 +1,40 @@
-# publicador_gstreamer.py
-import rospy
+# publicador_gstreamer_ros2.py
+
+import rclpy
+from rclpy.node import Node
 from sensor_msgs.msg import CompressedImage
 import numpy as np
 import cv2
 
-class GStreamerPublisher:
+class GStreamerPublisher(Node):
     def __init__(self):
-        self.sub = rospy.Subscriber("/image_raw/compressed", CompressedImage, self.callback, queue_size=1)
-        
-        # Define GStreamer pipeline (ajústalo a tu IP/puerto si es vía UDP)
-        gst_str = ('appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=ultrafast ! '
-                   'rtph264pay ! udpsink host=127.0.0.1 port=5000')
+        super().__init__('gstreamer_publisher_node')
+        self.subscription = self.create_subscription(
+            CompressedImage,
+            '/image_raw/compressed',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+        gst_str = (
+            'appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=ultrafast ! '
+            'rtph264pay ! udpsink host=127.0.0.1 port=5000'
+        )
         self.out = cv2.VideoWriter(gst_str, cv2.CAP_GSTREAMER, 0, 30, (640, 480), True)
 
-    def callback(self, msg):
+    def listener_callback(self, msg):
         np_arr = np.frombuffer(msg.data, np.uint8)
         image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
         if image_np is not None and image_np.shape[1] == 640 and image_np.shape[0] == 480:
             self.out.write(image_np)
 
-
 def main(args=None):
     rclpy.init(args=args)
-    image_send = GStreamerPublisher()
-    rclpy.spin(image_send)
-    image_send.destroy_node
-    rclpy.shutdown
+    node = GStreamerPublisher()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
-if __name__ == "__main__":
-    rospy.init_node("gstreamer_publisher_node")
-    GStreamerPublisher()
-    rospy.spin()
