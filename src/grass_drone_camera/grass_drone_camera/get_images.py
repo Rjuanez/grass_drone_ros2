267110bffd780836e7fa0360714beb89b40c8fa4
Bernard <bernardfg5@gmail.com>
2025-05-19 22:20:01 +0200

    millorar get_images

--- src/grass_drone_camera/grass_drone_camera/get_images.py
+++ src/grass_drone_camera/grass_drone_camera/get_images.py
@@ -12,34 +12,36 @@ class ImageSubscriber(Node):
         super().__init__("image_subscriber")
         self.subscription = self.create_subscription(Image, 'sky_cam', self.listener_callback, 10)
         self.br = CvBridge()
+        self.last_capture_time = self.get_clock().now()
+        self.capture_interval = rclpy.duration.Duration(seconds=5)  # Canvia això per ajustar l'interval
+
+        self.folder = "/home/ruben/Documents/grass_drone_ros2/src/grass_drone_camera/grass_drone_camera/Calibration_Photos2"
+        os.makedirs(self.folder, exist_ok=True)
 
     def listener_callback(self, data):
-        self.get_logger().info('Receiving video frame')
+        current_time = self.get_clock().now()
+        if (current_time - self.last_capture_time) < self.capture_interval:
+            return
+
         img = self.br.imgmsg_to_cv2(data, desired_encoding="rgb8")
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        img = cv2.resize(img,(900,580))
+        img = cv2.resize(img, (900, 580))
 
-        folder = "/home/ruben/Documents/grass_drone_ros2/src/grass_drone_camera/grass_drone_camera/Calibration_Photos2"
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = os.path.join(self.folder, f"capture_{timestamp}.png")
+        cv2.imwrite(filename, img)
+        self.get_logger().info(f"Imatge guardada: {filename}")
 
-        cv2.imshow("sky_cam",img)
-        key = cv2.waitKey(1) & 0xFF
-
-        if key == ord('c'):
-            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-            filename = os.path.join(folder, f"capture_{timestamp}.png")
-            cv2.imwrite(filename, img)
-            print(f"Imatge guardada: {filename}")
-
-        
-        
+        self.last_capture_time = current_time
 
 
 def main(args=None):
     rclpy.init(args=args)
     image_subscriber = ImageSubscriber()
     rclpy.spin(image_subscriber)
-    image_subscriber.destroy_node
-    rclpy.shutdown
+    image_subscriber.destroy_node()
+    rclpy.shutdown()
 
-if __name__ == 'main':
+
+if __name__ == '__main__':
     main()
